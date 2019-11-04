bc1582b9e12a527791eae34ff86de8e6abc148db
Shaneen Lasley <bluebeauty40@gmail.com>
2019-11-03 18:53:51 -0600

    changes to commit

--- src/main/java/com/shaneen/blog/blog/controllers/HomeController.java
+++ src/main/java/com/shaneen/blog/blog/controllers/HomeController.java
@@ -2,12 +2,17 @@ package com.shaneen.blog.blog.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @Controller
 public class HomeController{
-    @RequestMapping("/")
-    public String index() {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("msg", "Hello World!");
         return "index";
     }
 
